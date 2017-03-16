74bd4f5a72d5703b7cd56158f7d0e799d908740e
disword <1055619878@qq.com>
2017-03-16 18:12:28 +0800

    Update UsbPlugin.java

--- src/android/UsbPlugin.java
+++ src/android/UsbPlugin.java
@@ -4,6 +4,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbEndpoint;
